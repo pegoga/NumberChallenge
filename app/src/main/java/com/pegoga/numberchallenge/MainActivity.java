90febddb25b934c144e90e498d7d4e4602997148
agasca <alberto.gasca@pegoga.com>
2021-07-16 23:19:19 -0500

    Licencia

--- app/src/main/java/com/pegoga/numberchallenge/MainActivity.java
+++ app/src/main/java/com/pegoga/numberchallenge/MainActivity.java
@@ -1,5 +1,21 @@
 package com.pegoga.numberchallenge;
 
+/**
+ * ###Welcome
+ * Este un un codigo libre que aprendí en un curso en línea. Puedes copiarlo bajo la siguiente licencia BSD
+ *
+ * Copyright (c) <2021> alberto.gasca@pegoga.com. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted provided that the above copyright
+ * notice and this paragraph are duplicated in all such forms and that any documentation, advertising materials,
+ * and other materials related to such distribution and use acknowledge that the software was developed
+ * by pegoga The name of the may not be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED “AS IS” AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * */
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
